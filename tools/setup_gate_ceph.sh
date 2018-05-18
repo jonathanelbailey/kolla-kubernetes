03a9a67c1ffac11ea9ca90eba6ca9c133453f259
Jonathan Bailey <bailey.jonathan@hotmail.com>
2018-05-17 23:01:33 -0500

    updated ovsctl so it doesn't fail when a bridge exists

--- tools/setup_gate_ceph.sh
+++ tools/setup_gate_ceph.sh
@@ -27,7 +27,7 @@ if [ "x$CONFIG" == "xexternal-ovs" ]; then
     sudo rpm -Uvh https://repos.fedorapeople.org/openstack/openstack-newton/rdo-release-newton-4.noarch.rpm || true
     sudo yum install -y openvswitch
     sudo systemctl start openvswitch
-    sudo ovs-vsctl add-br br-ex
+    sudo ovs-vsctl add-br br-ex --may-exist
 fi
 
 tests/bin/setup_config.sh "$2" "$4" "$BRANCH"
